37eaf2ff66b30a0eab52d18e41516d30bc512a00
Cooper Raterink <Cooper Raterink>
2017-08-13 20:20:01 -0700

    collab filt cost gradients

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,21 +40,23 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% A. Compute cost
 hyp = X*Theta';
-J = 1/2*sum(sum(R.*(hyp - Y).^2));
+diff = hyp - Y;
+masked_diff = R.*diff;
+J = 1/2*sum(sum(masked_diff.^2));
 
+% B. Compute cost gradient w.r.t. movie features
+masked_diff_3d = permute(repmat(masked_diff, 1, 1, num_features), [1 3 2]);
+theta_rep_3d = permute(repmat(Theta, 1, 1, num_movies), [3 2 1]);
+mult_pre_sum = masked_diff_3d .* theta_rep_3d;
+X_grad = sum(mult_pre_sum, 3);
 
-
-
-
-
-
-
-
-
-
-
-
+% C. Compute cost gradient w.r.t. user features
+masked_diff_3d = permute(masked_diff_3d, [3 2 1]);
+x_rep_3d = permute(repmat(X, 1, 1, num_users), [3 2 1]);
+mult_pre_sum = masked_diff_3d .* x_rep_3d;
+Theta_grad = sum(mult_pre_sum, 3);
 
 
 % =============================================================
